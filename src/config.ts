a86c783114f1fe537f63c76caf4a6ea83d4d26e2
NotHarshhaa <reddyharshhaa12@gmail.com>
2024-06-08 13:07:09 +0530

    fix: `ogImage` is properly loaded when sharing posts on social platforms
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ export const SITE: Site = {
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  url: "https://prodevopsguy.tech/", // Add this line
 };
 
 export const LOCALE = {
